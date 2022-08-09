e1c414063384d86c9c1dbaa0161c31362bd54b60
pspyoung <pspyoung2@gmail.com>
2022-08-08 20:59:03 -0700

    Installs MUI, BrowserRouter, begins Navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import AppBar from '@mui/material/AppBar';
 
 function Navigation(props) {
-    return (
-        <div>
-            Test Nav
-        </div>
-    );
+	return (
+		<div>
+			<AppBar> Hi </AppBar>
+		</div>
+	);
 }
 
 export default Navigation;

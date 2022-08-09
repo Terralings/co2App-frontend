c42e8ca0755a50ca81b08765be87df8251762237
pspyoung <pspyoung2@gmail.com>
2022-08-09 13:46:30 -0700

    Styles Dashboard and Co2Emissions components

--- src/components/Co2Emission/Co2Emission.js
+++ src/components/Co2Emission/Co2Emission.js
@@ -1,11 +1,40 @@
 import React from 'react';
+import './Co2Emissions.css';
+import { Button } from '@mui/material';
+import Link from '@mui/material/Link';
 
-function Co2Emission(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Co2Emission() {
+	const commonStyles = {
+		bgcolor: 'background.paper',
+		borderColor: 'text.primary',
+		m: 1,
+		border: 1,
+		width: '20rem',
+		height: '20rem',
+	};
+	return (
+		<div>
+			<div className="circleContainer">
+				{/* <div className="circleContaner">
+                            <Box sx={{ justifyContent: 'center' }}>
+                                <Box sx={{ ...commonStyles, borderRadius: '50%' }}>
+                                    <div className="circle">
+                                        <h1> 6.8</h1> <h2>Tons of Co2</h2>
+                                    </div>
+                                </Box>
+                            </Box>
+                        </div> */}
+				<h1> 6.8</h1> <h2>Tons of Co2</h2>
+			</div>
+			<Button color="primary" variant="contained">
+				Save Result
+			</Button>
+
+			<p>
+				<Link href="#">Learn More</Link>
+			</p>
+		</div>
+	);
 }
 
 export default Co2Emission;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,49 @@
 import React from 'react';
+import './Dashboard.css';
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Paper,
+	Typography,
+} from '@mui/material';
 
 function Dashboard(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+	return (
+		<div>
+			<h4>Past Trips</h4>
+			<TableContainer
+				className="tableContainer"
+				sx={{ maxWidth: '95%', boxShadow: 3 }}
+				component={Paper}
+				align="center">
+				<Table aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Co2 Emissions:</TableCell>
+							<TableCell>Date:</TableCell>
+						</TableRow>
+					</TableHead>
+
+					<TableBody
+						sx={{
+							color: 'text.primary',
+							justifyContent: 'center',
+						}}>
+						<TableRow>
+							<TableCell>Text </TableCell>
+
+							<TableCell>Text </TableCell>
+						</TableRow>
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</div>
+	);
 }
 
 export default Dashboard;

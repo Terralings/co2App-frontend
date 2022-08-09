789773195e78de9200527168be068947927dfd09
jcheng1022 <jackycheng1022@gmail.com>
2022-08-09 11:13:35 -0400

    google oAuth

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+//FIREBASE STUFF;  MOVE IT TO RESPECTIVE LOCATION AFTERWARDS
+import { login, logout } from "../src/services/firebase";
+import { auth } from "./services/firebase";
 
 function App() {
+  const [user, setUser] = useState(null);
+  const [entry, setEntry] = useState([]);
+
+  const URL = "http://localhost:4000/home/";
+
+  // GET ROUTE
+  const getEntry = async () => {
+    const token = await user.getIdToken();
+    // console.log(token);
+    const response = await fetch(URL, {
+      method: "GET",
+      headers: {
+        Authorization: " Bearer  " + token,
+      },
+    });
+    const data = await response.json();
+    setEntry(data);
+  };
+
+  // CREATE ROUTE
+
+  const createEntry = async (entry) => {
+    const token = await user.getIdToken();
+    await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-type": "Application/json",
+        Authorization: " Bearer  " + token,
+      },
+      body: JSON.stringify(entry),
+    });
+  };
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => setUser(user));
+    getEntry();
+    return () => {
+      unsubscribe();
+    };
+  }, [user]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <div onClick={logout}>Logout</div>
+      ) : (
+        <div onClick={login}>Login</div>
+      )}
     </div>
   );
 }

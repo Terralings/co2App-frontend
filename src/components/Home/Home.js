d660f040ac13518627c8f4f31f7b72b7fc67238f
mmarvin3 <mmerida336@gmail.com>
2022-08-10 14:14:40 -0400

    styling updates to home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,68 +1,71 @@
 import { useState } from "react";
+import './Home.css'
 
 function Home(props) {
-  const [newForm, setNewForm] = useState({
-    pointA: "",
-    pointB: "",
-    vehTypeInput: "",
-    distance: "",
-  });
-
-  const handleChange = (event) => {
-    setNewForm({
-      ...newForm,
-      [event.target.name]: event.target.value,
+    const [newForm, setNewForm] = useState({
+        pointA: "",
+        pointB: "",
+        vehTypeInput: "",
+        distance: "",
     });
-  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.createEntry(newForm); //not sure about this one, need confirmation
-  };
+    const handleChange = (event) => {
+        setNewForm({
+            ...newForm,
+            [event.target.name]: event.target.value,
+        });
+    };
 
-  const [showForm, setShowForm] = useState(true);
-  // const [showCreateButton, setShowCreateButton] = useState(true)
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        props.createEntry(newForm); //not sure about this one, need confirmation
+    };
 
-  function submitAction() {
-    setShowForm(false);
-    setShowForm(true);
-    // setShowCreateButton(true);
-  }
+    const [showForm, setShowForm] = useState(true);
+    // const [showCreateButton, setShowCreateButton] = useState(true)
 
-  function createAction() {
-    setShowForm(true);
-    // setShowCreateButton(false);
-  }
+    function submitAction() {
+        setShowForm(false);
+        setShowForm(true);
+        // setShowCreateButton(true);
+    }
 
-  return (
-    <div>
-      <section>
-        <h1>Zero Carbon</h1>
-        {showForm ? (
-          <form className="co2Form" onClick={handleSubmit}>
-            <div className="field-inline">
-              <h2 className="form-label">Vehicle Type</h2>
-              <select
-                name={newForm.vehTypeInput}
-                onChange={(event) => setNewForm(event.target.value)}
-              >
-                <option value="SmallDieselCar">Compact Car</option>
-                <option value="MediumDieselCar">Mid/Full Size Sedan</option>
-                <option value="LargeDieselCar">Truck/SUV</option>
-                <option value="MediumHybridCar">Hybrid Car</option>
-                <option value="LargeHybridCar">Hybrid SUV</option>
-              </select>
-            </div>
-            <div className="field-inline">
-              <h2 className="form-label">Trip Distance(km)</h2>
-              <input
-                value={newForm.distance}
-                onChange={handleChange}
-                name="distance"
-                type="text"
-              />
-            </div>
-            {/* <div className="field-inline">
+    function createAction() {
+        setShowForm(true);
+        // setShowCreateButton(false);
+    }
+
+    return (
+        <div>
+            <section>
+                <h1 className="appName">Zero Carbon</h1>
+                {showForm ? (
+                    <form className="co2Form" onClick={handleSubmit}>
+                        <div className="field-inline">
+                            {/* <h2 className="form-label">Vehicle Type</h2> */}
+                            <select
+                                className="dropdown"
+                                name={newForm.vehTypeInput}
+                                onChange={(event) => setNewForm(event.target.value)}
+                            >
+                                <option value="SmallDieselCar">Compact Car</option>
+                                <option value="MediumDieselCar">Mid/Full Size Sedan</option>
+                                <option value="LargeDieselCar">Truck/SUV</option>
+                                <option value="MediumHybridCar">Hybrid Car</option>
+                                <option value="LargeHybridCar">Hybrid SUV</option>
+                            </select>
+                        </div>
+                        <div className="field-inline">
+                            <h2 className="form-label">Trip Distance(km)</h2>
+                            <input
+                                className="userDistance"
+                                value={newForm.distance}
+                                onChange={handleChange}
+                                name="distance"
+                                type="text"
+                            />
+                        </div>
+                        {/* <div className="field-inline">
                             <h2 className='form-label'>Start Point</h2>
                             <input
                                 value={newForm.pointA}
@@ -82,14 +85,14 @@ function Home(props) {
                                 placeholder='(ex. 321 South Main Rd...)'
                             />
                         </div> */}
-            <button className="create" onClick={() => submitAction()}>
-              Submit
-            </button>
-          </form>
-        ) : null}
-      </section>
-    </div>
-  );
+                        <button className="seeCarbonbutton" onClick={() => submitAction()}>
+                            Submit
+                        </button>
+                    </form>
+                ) : null}
+            </section>
+        </div>
+    );
 }
 
 export default Home;

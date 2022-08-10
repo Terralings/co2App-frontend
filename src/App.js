f15a14ba90907b39786a7a2a490a94ce796e91a9
jcheng1022 <jackycheng1022@gmail.com>
2022-08-09 23:32:28 -0400

    adds API result display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import Navigation from "./components/Navigation/Navigation";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Co2Emission from "./components/Co2Emission/Co2Emission";
 import Dashboard from "./components/Dashboard/Dashboard";
+import axios from "axios";
 //FIREBASE STUFF;  MOVE IT TO RESPECTIVE LOCATION AFTERWARDS
 import { login, logout } from "../src/services/firebase";
 import { auth } from "./services/firebase";
@@ -16,6 +17,50 @@ function App() {
 
   const URL = "http://localhost:4000/home/";
 
+  //carbon states
+  // const [distInput, setDistInput] = useState("");
+  // const [vehTypeInput, setVehTypeInput] = useState("");
+  const [carbonInfo, setCarbonInfo] = useState("");
+
+  // FORM STATES
+  const [newForm, setNewForm] = useState({
+    // pointA: "",
+    // pointB: "",
+    vehTypeInput: "",
+    distance: "",
+  });
+
+  //API FUNCTION
+  const getAPI = async () => {
+    axios({
+      method: "post",
+      url: "https://app.trycarbonapi.com/api/carTravel",
+      headers: {
+        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiODJiMjhjNWUyYmNkYjFhMDVlZTg1OGEwMzgyNGEwZDEyMDE5MWM3NGM5MDU5NzZkMmQ3NzBmZGRjNGEyZDk1YTg0ZTcyNDc1OGM1YmU2MTciLCJpYXQiOjE2NjAwOTk5NjQsIm5iZiI6MTY2MDA5OTk2NCwiZXhwIjoxNjkxNjM1OTY0LCJzdWIiOiIxMTU0Iiwic2NvcGVzIjpbXX0.odPwnPg9Lqq0TcJRVb4szjVneOl4SXTnu8aTRedpLfzez9riAMU6yGoQP5p1XP-84mqwEyZTDpO-aOe7T1M5_A`,
+        "Content-Type": "application/json",
+      },
+      data: {
+        distance: newForm.distance,
+        vehicle: newForm.vehTypeInput,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        const apiInfo = res.data;
+        const { carbon } = apiInfo;
+        setCarbonInfo(carbon);
+        console.log(carbonInfo);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  const navigate = useNavigate();
+  const handleCalcSubmit = (e) => {
+    e.preventDefault();
+    getAPI();
+    navigate("/co2Emissions");
+  };
   // GET ROUTE
   const getEntry = async () => {
     const token = await user.getIdToken();
@@ -59,9 +104,27 @@ function App() {
       )}
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              // carbonInfo={carbonInfo}
+              // setCarbonInfo={setCarbonInfo}
+              // distInput={distInput}
+              // setDistInput={setDistInput}
+              // vehTypeInput={vehTypeInput}
+              // setVehTypeInput={setVehTypeInput}
+              newForm={newForm}
+              setNewForm={setNewForm}
+              handleCalcSubmit={handleCalcSubmit}
+            />
+          }
+        />
         <Route path="/about" element={<About />} />
-        <Route path="/co2emissions" element={<Co2Emission />} />
+        <Route
+          path="/co2emissions"
+          element={<Co2Emission carbonInfo={carbonInfo} user={user} />}
+        />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,68 +1,94 @@
 import { useState } from "react";
 
 function Home(props) {
-  const [newForm, setNewForm] = useState({
-    pointA: "",
-    pointB: "",
-    vehTypeInput: "",
-    distance: "",
-  });
+  //   const [newForm, setNewForm] = useState({
+  //     // pointA: "",
+  //     // pointB: "",
+  //     vehTypeInput: "",
+  //     distance: "",
+  //   });
 
-  const handleChange = (event) => {
-    setNewForm({
-      ...newForm,
+  //   const handleChange = (event) => {
+  //     setNewForm({
+  //       ...newForm,
+  //       [event.target.name]: event.target.value,
+  //     });
+  //   };
+
+  const handleDistChange = (event) => {
+    props.setDistInput(event.target.value);
+  };
+  const handleVehChange = (event) => {
+    // props.setVehTypeInput(event.target.value);
+    props.setNewForm({
+      ...props.newForm,
       [event.target.name]: event.target.value,
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    props.createEntry(newForm); //not sure about this one, need confirmation
-  };
-
-  const [showForm, setShowForm] = useState(true);
-  // const [showCreateButton, setShowCreateButton] = useState(true)
-
-  function submitAction() {
-    setShowForm(false);
-    setShowForm(true);
-    // setShowCreateButton(true);
-  }
-
-  function createAction() {
-    setShowForm(true);
-    // setShowCreateButton(false);
-  }
+  //   const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     props.createEntry(newForm); //not sure about this one, need confirmation
+  //   };
 
   return (
     <div>
       <section>
         <h1>Zero Carbon</h1>
-        {showForm ? (
-          <form className="co2Form" onClick={handleSubmit}>
-            <div className="field-inline">
-              <h2 className="form-label">Vehicle Type</h2>
-              <select
-                name={newForm.vehTypeInput}
-                onChange={(event) => setNewForm(event.target.value)}
+        <form className="co2Form" onSubmit={props.handleCalcSubmit}>
+          <div className="field-inline">
+            <h2 className="form-label">Vehicle Type</h2>
+            <select
+              name={props.newForm.vehTypeInput}
+              onChange={(event) =>
+                props.setNewForm({
+                  ...props.newForm,
+                  vehTypeInput: event.target.value,
+                })
+              }
+            >
+              <option
+                // onSelect={(e) => setNewForm(e.target.value)}
+                value="SmallDieselCar"
               >
-                <option value="SmallDieselCar">Compact Car</option>
-                <option value="MediumDieselCar">Mid/Full Size Sedan</option>
-                <option value="LargeDieselCar">Truck/SUV</option>
-                <option value="MediumHybridCar">Hybrid Car</option>
-                <option value="LargeHybridCar">Hybrid SUV</option>
-              </select>
-            </div>
-            <div className="field-inline">
-              <h2 className="form-label">Trip Distance(km)</h2>
-              <input
-                value={newForm.distance}
-                onChange={handleChange}
-                name="distance"
-                type="text"
-              />
-            </div>
-            {/* <div className="field-inline">
+                Compact Car
+              </option>
+              <option
+                // onSelect={(e) => handleVehChange(e.target.value)}
+                value="MediumDieselCar"
+              >
+                Mid/Full Size Sedan
+              </option>
+              <option
+                // onSelect={(e) => handleVehChange(e.target.value)}
+                value="LargeDieselCar"
+              >
+                Truck/SUV
+              </option>
+              <option
+                // onSelect={(e) => handleVehChange(e.target.value)}
+                value="MediumHybridCar"
+              >
+                Hybrid Car
+              </option>
+              <option
+                // onSelect={(e) => handleVehChange(e.target.value)}
+                value="LargeHybridCar"
+              >
+                Hybrid SUV
+              </option>
+            </select>
+          </div>
+          <div className="field-inline">
+            <h2 className="form-label">Trip Distance(km)</h2>
+            <input
+              value={props.newForm.distance}
+              onChange={handleVehChange}
+              name="distance"
+              type="text"
+            />
+          </div>
+          {/* <div className="field-inline">
                             <h2 className='form-label'>Start Point</h2>
                             <input
                                 value={newForm.pointA}
@@ -82,11 +108,8 @@ function Home(props) {
                                 placeholder='(ex. 321 South Main Rd...)'
                             />
                         </div> */}
-            <button className="create" onClick={() => submitAction()}>
-              Submit
-            </button>
-          </form>
-        ) : null}
+          <input type="submit" />
+        </form>
       </section>
     </div>
   );
